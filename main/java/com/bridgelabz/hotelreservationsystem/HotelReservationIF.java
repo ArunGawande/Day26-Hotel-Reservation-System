0420788c457ba48405d31fa90ec77f9d997e1353
Arun <arungawande1995@gmail.com>
2022-04-19 13:43:55 +0530

    Refactor : return string from get cheapest hotel

--- main/java/com/bridgelabz/hotelreservationsystem/HotelReservationIF.java
+++ main/java/com/bridgelabz/hotelreservationsystem/HotelReservationIF.java
@@ -4,10 +4,10 @@ import java.time.LocalDate;
 import java.util.ArrayList;
 
 public interface HotelReservationIF
-{
-    public void addHotel(String hotelName,int rating, double weekdayRegularCustomerCost, double weekendRegularCustomerCost);
+{public void addHotel(String hotelName,int rating, double weekdayRegularCustomerCost, double weekendRegularCustomerCost);
     public int getHotelListSize();
     public void printHotelList();
     public ArrayList<Hotel> getHotelList();
-    public Hotel getCheapestHotel(LocalDate startDate, LocalDate endDate);
+    public String getCheapestHotel(LocalDate startDate, LocalDate endDate);
 }
+
